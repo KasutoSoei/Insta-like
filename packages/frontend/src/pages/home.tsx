fabed5077db364f9f2894cdf6be62df48d2a3f7c
Lina <144618594+rinrina01@users.noreply.github.com>
2026-02-10 10:18:20 +0100

    / stabilized homepage

--- packages/frontend/src/pages/home.tsx
+++ packages/frontend/src/pages/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+//import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ChihuaWow" },
+    { name: "description", content: "Welcome to ChihuaWow!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <p> hello there </p>// <Welcome />;
 }

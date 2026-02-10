bc5c74e72202df3fe94b6b5954fdf925b0fc0e18
Lina <144618594+rinrina01@users.noreply.github.com>
2026-02-10 12:11:11 +0100

    feat: add register page route and base header

---
+++ frontend/src/pages/auth/register.tsx
@@ -0,0 +1,14 @@
+export default function Register() {
+  return (
+    <main>
+      <div>
+        <p>See everyday moments from your close friends.</p>
+      </div>
+
+
+      <div>
+        
+      </div>
+    </main>
+  )
+}

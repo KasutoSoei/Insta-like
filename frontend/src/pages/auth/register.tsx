50f6ebe3c7cc69a004d596ad50c8625cc0667bf3
Lina <144618594+rinrina01@users.noreply.github.com>
2026-02-10 12:52:11 +0100

    feat: register form skeleton (type validation with Zod and React Hook Forms)

--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -1,14 +1,72 @@
+import { useForm, type SubmitHandler, Controller } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import z from "zod";
+
+const RegisterUserSchema = z
+  .object({
+    email: z
+      .email()
+      .min(1, { message: 'Please enter an email address' }),
+    password: z
+      .string()
+      .min(1, { message: 'Please enter a password' })
+      .min(6, { message: 'Password must be at least 6 characters' }),
+    confirmPassword: z
+      .string()
+      .min(1, { message: 'Please confirm your password' })
+      .min(6, { message: 'Password must be at least 6 characters' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
+
+type RegisterUserSchemaType = z.infer<typeof RegisterUserSchema>;
+
 export default function Register() {
+  const form = useForm<RegisterUserSchemaType>({
+    resolver: zodResolver(RegisterUserSchema),
+    defaultValues:{email:"", password:"", confirmPassword:""}, 
+  });
+
+  const onSubmit: SubmitHandler<RegisterUserSchemaType> = async (data) => {
+    try {
+      //await registerAccount(data);
+      //await loginAccount(data);
+      //router.push('/weeklyPicks');
+    } catch (error) {
+      console.error('Registration Failed', error);
+    }
+  };
+
   return (
-    <main>
-      <div>
-        <p>See everyday moments from your close friends.</p>
-      </div>
+    <form onSubmit={form.handleSubmit(onSubmit)}>
+      <Controller
+        render={({ field }) => <input placeholder="Enter your email" {...field} />}
+        name="email"
+        control={form.control}
+      />
+      <div>{form.formState.errors.email?.message}</div>
 
+      <Controller
+        render={({ field }) => <input type="password" placeholder="Enter your password" {...field} />}
+        name="password"
+        control={form.control}
+      />
 
-      <div>
-        
-      </div>
-    </main>
-  )
+      <div>{form.formState.errors.password?.message}</div>
+
+      <Controller
+        render={({ field }) => <input type="password" placeholder="Confirm your password" {...field} />}
+        name="password"
+        control={form.control}
+      />
+
+      <div>{form.formState.errors.confirmPassword?.message}</div>
+
+      <input type="submit" />
+    </form>
+  );
 }
+
+// form.formState.errors.confirmPassword.message
